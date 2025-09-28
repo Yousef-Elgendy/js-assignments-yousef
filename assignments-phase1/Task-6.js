e8607e595d522ce2d31990b889eb5961a2cf7c65
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 21:02:46 +0300

    add task 6 solution

---
+++ assignments-phase1/Task-6.js
@@ -0,0 +1,16 @@
+// Name: Yousef Elgendy
+// Task 5:  FizzBuzz
+// Description: print number from 1 to 100 and do some operation on it 
+
+for (let i = 1; i <= 100; ++i){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    }else if(i % 3 === 0){
+        console.log("Fizz");
+    }else if(i % 3 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
+
