aa15c5dc7b75cb6ecda3d80ba2a232aeaf6943b1
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 18:51:54 +0300

    add task 1 solution

---
+++ assignments-phase1/Task-1.js
@@ -0,0 +1,6 @@
+// Name: Yousef Elgendy
+// Task 1: String Length
+// Description: Asking user to input any sentence then print character length of this sentence
+
+let sentence = prompt("Enter any sentence");
+console.log(sentence.length);
