19c63272da41523bab223c1428085cbd94dcb531
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 22:44:52 +0300

    add bonus 4 task solution

---
+++ assignments-phase1/Bonus-4.js
@@ -0,0 +1,9 @@
+// Name: Yousef Elgendy
+// Bonus 4: Minimum Number Finder
+// Description: Ask the user to enter 3 numbers and print the smallest one
+
+let firstNum = prompt("Enter first number");
+let secondNum = prompt("Enter second number");
+let thirdNum = prompt("Enter third number");
+
+alert(`the smallest number is ${ Math.min(firstNum, secondNum, thirdNum)}`);
