dfcad0983c6e2cd2499cee91fa4be59b3108c1f7
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 20:11:29 +0300

    add Task 2 solution

---
+++ assignments-phase1/Task-2.js
@@ -0,0 +1,18 @@
+// Name: Yousef Elgendy
+// Task 2: Number Operations
+// Description: Print the results of performing addition, subtraction, multiplication, and division on two numbers.
+
+
+
+doBasicOperations(5, 3);
+doBasicOperations(10, 2);
+doBasicOperations(20, 4);
+
+function doBasicOperations(firstNum, SecondNum) {
+    console.log(`Addition of ${firstNum} + ${SecondNum} =  ${firstNum + SecondNum}`);
+    console.log(`Subtraction of ${firstNum} - ${SecondNum} =  ${firstNum - SecondNum}`);
+    console.log(`Multiplication of ${firstNum} * ${SecondNum} =  ${firstNum * SecondNum}`);
+    console.log(`Division of ${firstNum} / ${SecondNum} =  ${firstNum / SecondNum}`);
+}
+
+
