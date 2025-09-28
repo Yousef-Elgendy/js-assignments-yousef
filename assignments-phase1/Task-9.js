5d7a7a2ce0b9a9b40582496cddd40a068df91fec
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 21:45:30 +0300

    add task 9 solution

---
+++ assignments-phase1/Task-9.js
@@ -0,0 +1,13 @@
+// Name: Yousef Elgendy
+// Task 8: Conditional Execution
+// Description: Ask user for their age and print "Child" if under 13, "Teenager" if 13 to 19, or "Adult" if 20 or older.
+
+let userAge = prompt("Enter your age :");
+
+if(userAge < 13){
+    console.log("Child");
+}else if(userAge <= 19){
+    console.log("Teenager");
+}else {
+    console.log("Adult");
+}
