0a4f0127099feb9108f57a52697ea24178b8f4d7
Yousef-Elgendy <YousefElgendy.Dev@gmail.com>
2025-09-28 22:28:43 +0300

    add bonus 2 task solution

---
+++ assignments-phase1/Bonus-2.js
@@ -0,0 +1,10 @@
+// Name: Yousef Elgendy
+// Bonus 2: Multiplication Table
+// Description: Print the multiplication table from 1 to 10 using nested loops.
+
+for (let i = 1; i <= 10; ++i){
+    for(let j = 1; j <= 12; ++j){
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log("-------------------------------------------");
+}
